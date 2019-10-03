92b761cd38dadc3c3d88805446728cd00043636b
Angel Suarez Perez <angelsupe5@gmail.com>
2019-10-03 13:22:55 +0200

    Mi ciudad de nacimiento es Leon

--- Prueba.java
+++ Prueba.java
@@ -3,6 +3,8 @@ public class miprimeraclase {
  public static void main(String args[]) {
 
   System.out.println("Hola Angel Suarez Perez");
+  System.out.println("Mi ciudad de nacimiento es Leon");
+  System.out.println("Vives en Leon");
  }
 
 }
